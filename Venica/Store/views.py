756af2c8a88ed7b374bdf0d30f10b885d80e9ccf
janet <janetmutua04@gmail.com>
2022-06-05 13:10:28 +0300

    Feat:(update)initialized templates

--- Venica/Store/views.py
+++ Venica/Store/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def Storefront(request):
     return render(request, 'Store/index.html')
+
+
+def Shop(request):
+    return render(request, 'Store/shop.html')
+
+
+def Product_detail(request):
+    return render(request, 'Store/shopdetail.html')
+
+def Wishlist(request):
+    return render(request, 'Store/wishlist.html')
+
+
+def Checkout(request):
+    return render(request, 'Store/checkout.html')
+
+def AddCart(request):
+    return render(request, 'Store/cart.html')
